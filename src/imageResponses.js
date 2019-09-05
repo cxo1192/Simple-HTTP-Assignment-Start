cdb87a0197d847dd4d9f27a2fb72435b011e8a20
cxo1192 <cxo1192@g.rit.edu>
2019-09-04 20:47:46 -0400

    made simple HTTP server with text image json and html handlers

---
+++ src/imageResponses.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const sponge = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+
+const getSponge = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(sponge);
+  response.end();
+};
+
+
+module.exports.getSponge = getSponge;
